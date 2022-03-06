842b28d85119023416c57c1d635a7a2d4b648de5
S4muk4SDF <sdf.dias.faria@gmail.com>
2022-03-05 21:19:59 -0300

    Projeto reformado

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 function redirecionamentoWhats (){
     let numero = document.getElementById("number").value
+    let dd = document.getElementById("dd").value
 
-    window.open (`https://api.whatsapp.com/send/?phone=55${numero}`, '_blank')
+    window.open (`https://api.whatsapp.com/send/?phone=55${dd + numero}`, '_blank')
 
 }
 
 document.addEventListener("keypress", function(e) {
     if (e.key === "Enter") {
         let numero = document.getElementById("number").value
-        window.open (`https://api.whatsapp.com/send/?phone=55${numero}`, '_blank')
+        let dd = document.getElementById("dd").value
+        window.open (`https://api.whatsapp.com/send/?phone=55${dd + numero}`, '_blank')
     }
 })
 
